f7fef0c3f142e10c00d031123a1697b3a5ffaf7d
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-03-31 12:09:31 -0700

    client necceserary files added

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
