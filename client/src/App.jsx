75066e5debb0c38771d0c6403f02bd6e70a750f7
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-03-31 12:50:28 -0700

    Binding the components in App file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
-import {useEffect} from 'react';
+import { LoginPage } from './pages/LoginPage';
+import { TodoPage } from './pages/TodoPage';
+
+export const TokenContext = React.createContext(null);
+
+const ProtectedRoute = ({ element }) => {
+  const [token] = useContext(TokenContext);
+  return token ? element() : <Navigate to="/login" />;
+};
 
 function App() {
-
-  useEffect(() => {
-    console.log('we are here');
-  }, [])
+  const [token, setToken] = useState(null);
 
   return (
     <div className="App">
-      <h1>Hello, World!!!</h1>
+      <TokenContext.Provider value={[token, setToken]}>
+        <Routes>
+          <Route
+            path="/"
+            element={<ProtectedRoute element={TodoPage} />}
+          />
+          <Route path="login" element={<LoginPage />} />
+        </Routes>
+      </TokenContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
