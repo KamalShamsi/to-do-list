b2f537fee14353ec985822152ca73366dbc3657b
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-03-31 12:42:07 -0700

    readTodosRequest added and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    console.log('we are here');
+  }, [])
+
   return (
     <div className="App">
+      <h1>Hello, World!!!</h1>
     </div>
   )
 }
